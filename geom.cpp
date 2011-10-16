2a4073771988a753bd430539b1907070802aea30
Marek Rogalski <mafikpl@gmail.com>
2011-10-16 18:30:43 +0200

    Znajdowanie najbliższej pary punktów

--- geom.cpp
+++ geom.cpp
@@ -1,5 +1,9 @@
 #include "geom.hpp"
 
+Pt buffer[1000000];
+int ind[100000];
+Pt hull[1000000];
+
 int main(int argc, char *argv[]) {
   int n;
   scanf("%d", &n);
@@ -12,7 +16,10 @@ int main(int argc, char *argv[]) {
 
   int hull_size = graham_hull(hull, n);
   printf("Graham: OK (%d punktów w otoczce)\n", hull_size);
-  printf("Powierzchnia: OK (%lld)\n", area(hull, hull_size));
-  
+  printf("Powierzchnia x 2: OK (%lld)\n", area(hull, hull_size));
+
+  closest_pair cp(buffer, n);
+  int min_dist = cp.calculate();
+  printf("Minimal distance^2: %d\n", min_dist);
   return 0;
 }
--- geom.hpp
+++ geom.hpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 #include <cmath>
+#include <climits>
+
 #include <algorithm>
 #include <cstring>
 
@@ -17,6 +19,9 @@ struct Pt {
   bool operator<(const Pt& v) const { return dir() < v.dir(); }
   Ft dist(const Pt& v) const {
     return sqrt((x-v.x)*(x-v.x) + (y-v.y)*(y-v.y));
+  }
+  Ft dist2(const Pt& v) const {
+    return (x-v.x)*(x-v.x) + (y-v.y)*(y-v.y);
   }
   It len2() const {
     return x*x+y*y;
@@ -78,6 +83,85 @@ int graham_hull(Pt* poly, int N) {
   return M+1;
 }
 
-Pt buffer[1000000];
-int ind[100000];
-Pt hull[1000000];
+struct by_x {
+  Pt* p;
+  by_x(Pt *p_):p(p_) {}
+  bool operator()(int a, int b) { return p[a].x < p[b].x; }
+};
+
+struct by_y {
+  Pt* p;
+  by_y(Pt *p_):p(p_) {}
+  bool operator()(int a, int b) { return p[a].y < p[b].y; }
+};
+
+struct closest_pair {
+  Pt *points;
+  int *xsort, *ysort;
+  int *sub, ssize;
+  Pt* A;
+  int amin, n;
+
+  closest_pair(Pt *pts, int n_): points(pts), xsort(new int[n_]), ysort(new int[n_]), sub(new int[n_]), A(pts), amin(INT_MAX), n(n_) {}
+  ~closest_pair() {
+    delete[] xsort, ysort, sub;
+  }
+
+  void rec(Pt& a, int len) {
+    if(len < amin) {
+      A = &a;
+      amin = len;
+    }
+  }
+
+  int calc_closest(int* xbegin, int* ybegin, int n) {
+    long long min = INT_MAX;
+    if(n <= 3) {
+      for (int i = 0; i<n; ++i) {
+	for (int j = i+1; j<n; ++j) {
+	  long long d = points[xbegin[i]].dist2(points[xbegin[j]]);
+	  if(d < min) {
+	    min = d;
+	    rec(points[xbegin[i]], min);
+	  }
+	}
+	ybegin[i] = xbegin[i];
+      }
+      std::sort(ybegin, ybegin+n, by_y(points));
+    } else {
+      int nl = (n+1)/2, nr = n/2;
+      int *xl = xbegin, *xr = xbegin + nl;
+      int *yl = ybegin, *yr = ybegin + nl;
+      long long minl = calc_closest(xl, yl, nl);
+      long long minr = calc_closest(xr, yr, nr);
+      min = std::min(minl, minr);
+      std::inplace_merge(ybegin, ybegin+nl, ybegin+n, by_y(points));
+
+      long l = points[xsort[nl]].x;
+      ssize = 0;
+      for(int i=0; i<n; ++i) {
+	long x = points[ybegin[i]].x;
+	if((x-l)*(x-l) < min) sub[ssize++] = ybegin[i];
+      }
+
+      for(int i=0; i<ssize; ++i) {
+	Pt& a = points[sub[i]];
+	for(int j=1; (j<=8) && ((i+j)<ssize); ++j) {
+	  long long dist = a.dist2(points[sub[i+j]]);
+	  if(dist < min) {
+	    min = dist;
+	    rec(points[sub[i]], min);
+	  }
+	}
+      }
+    }
+    return min;
+  }
+
+  int calculate() {
+    for (int i=0; i<n; ++i) xsort[i] = ysort[i] = i;
+    sort(xsort, xsort+n, by_x(points));
+    calc_closest(xsort, ysort, n);
+    return amin;
+  }
+};
